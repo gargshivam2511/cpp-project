7d738fc5c0f7462804aff41692c55c91d19c3bd7
mitshivam10 <mitshivam10@users.noreply.github.com>
2022-03-10 12:10:35 -0800

    small change

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <vector>
 #include "list.cpp"
+
 using namespace std;
+
+int getRadius(std::vector<int> houses, std::vector<int> lamps)
+{
+    // fill in
+    // with solution
+    int minRadius = 0;
+    for (int i = 0; i < houses.size(); i++)
+    {
+        int minH = 1000000;
+        for (int j = 0; j < lamps.size(); j++)
+        {
+            if (abs(lamps[j] - houses[i]) < minH)
+                minH = abs(lamps[j] - houses[i]);
+        }
+        if (minH > minRadius)
+        {
+            minRadius = minH;
+        }
+    }
+    return minRadius;
+}
 
 int main()
 {
@@ -9,5 +32,5 @@ int main()
     ls.push("b");
     ls.push("c");
     ls.push("d");
-    cout << "Hello" << ls.pop() << ls.pop() << ls.pop() << ls.pop() << ls.pop();
+    cout << "Hello" << ls.pop() << ls.pop() << ls.pop() << ls.pop() << getRadius({1, 3, 4}, {0, 2});
 }
